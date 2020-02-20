9f80f04dc40c430daeeef3f9771f64958e041750
chenggb0902 <chenggb0902@thundersoft.com>
2020-02-20 10:20:24 +0800

    update build gradle and some old codes

--- app/src/main/java/com/pythoncat/ipcorservice/activity/LauncherActivity.java
+++ app/src/main/java/com/pythoncat/ipcorservice/activity/LauncherActivity.java
@@ -1,8 +1,8 @@
 package com.pythoncat.ipcorservice.activity;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.apkfuns.logutils.LogUtils;
 import com.pythoncat.ipcorservice.R;
